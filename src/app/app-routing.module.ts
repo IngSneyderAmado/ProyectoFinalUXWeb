e5b4060a0954206dd7152fbded8e0478f40e8967
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-03-09 11:42:18 -0500

    se crea la ruta a iniciosesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciosesionComponent } from './components/iniciosesion/iniciosesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  IniciosesionComponent},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
