e49285b516355278fdf93eadf6e191d755100625
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-03-09 11:44:41 -0500

    se crea inicio sesion

--- src/app/components/iniciosesion/iniciosesion.component.ts
+++ src/app/components/iniciosesion/iniciosesion.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-iniciosesion',
   templateUrl: './iniciosesion.component.html',
   styleUrls: ['./iniciosesion.component.css']
 })
-export class IniciosesionComponent {
+export class IniciosesionComponent implements OnInit  {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  irAHome(): void {
+    this.router.navigate(['/home']);
+  }
 
 }
+
